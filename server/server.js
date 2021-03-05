94091400227c8690a3b6b3a8107e1513ffdfa5cf
gretaivan <greta.ivan91@gmail.com>
2021-03-05 11:56:02 +0000

    completes setup of the project

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
---
+++ server/test/api.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+// import server
+const server = require('../server');
+
+describe('API server', () => {
+
+
+    beforeAll(() => {
+        // start the server and store it in the api variable
+        api = server.listen(5000, () =>
+            console.log('Test server running on port 5000')
+        );
+    });
+
+    afterAll((done) => {
+        // close the server, then run done
+        console.log('Gracefully stopping test server');
+        api.close(done);
+    });
+});
